382fc0b83e8c91af2bfaeb364979fcab9d855149
Gabriele0Brancato <160317734+Gabriele0Brancato@users.noreply.github.com>
2024-05-04 11:20:34 +0200

    Add files via upload

---
+++ PYAPI.py/main.py
@@ -0,0 +1,58 @@
+"""
+At the command line, only need to run once to install the package via pip:
+
+$ pip install google-generativeai
+"""
+
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyAdrGWUCwF2pr-1DWwdfgVmZmqha14ceXo")
+
+# Set up the model
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 0,
+  "max_output_tokens": 8192,
+}
+
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+]
+
+model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
+
+convo = model.start_chat(history=[])
+
+while True:
+  # Get user input from terminal
+  user_input = input("Your Input: ")
+
+  # Send user input to the model
+  convo.send_message(user_input)
+
+  # Print the model response
+  print(convo.last.text)
+
+  # Exit loop if user enters 'quit'
+  if user_input.lower() == 'quit':
+    break
+
+print("Conversation ended. Thank you!")
